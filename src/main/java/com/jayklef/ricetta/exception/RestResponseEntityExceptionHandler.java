857a011eee2da895a144dece06c2c1425fa009c7
Jayklef <jerryarhawho@gmail.com>
2022-01-22 16:40:40 +0100

    Added ResponseEntityExceptionHandler and ExceptionHandler classes

--- src/main/java/com/jayklef/ricetta/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/jayklef/ricetta/exception/RestResponseEntityExceptionHandler.java
@@ -1,10 +1,45 @@
 package com.jayklef.ricetta.exception;
 
+import com.jayklef.ricetta.model.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 @ResponseStatus
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ClientNotFoundException.class)
+    public ResponseEntity<ErrorMessage> ClientNotFoundException(ClientNotFoundException exception,
+                                                                WebRequest request){
+        ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND,
+                exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(message);
+    }
+
+    @ExceptionHandler(MovieNotFoundException.class)
+    public ResponseEntity<ErrorMessage> MovieNotFoundException(MovieNotFoundException exception,
+                                                               WebRequest request){
+        ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND,
+                exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(message);
+    }
+
+    @ExceptionHandler(GenreNotFoundException.class)
+    public ResponseEntity<ErrorMessage> GenreNotFoundException(GenreNotFoundException exception,
+                                                               WebRequest request){
+        ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND,
+                exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(message);
+    }
 }
